f6393e53d6cfb0aecc3ef15b8d07ee25086d3f59
hashtagnosweat <ridorachmatan@gmail.com>
2023-03-07 09:58:12 +0700

    another minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,14 @@ const Home = () => {
       name="home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-20"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-between h-full px-4 md:flex-row">
 
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-between h-full px-4 md:flex-row">
         <div className="hidden md:flex flex-col justify-center items-center mt-12">
           <div className="w-5 h-5 rounded-full bg-blue-700" />
           <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-blue-700 to-transparent" />
         </div>
 
-        <div className="flex flex-col justify-center h-full mr-10">
+        <div className="flex flex-col justify-center h-full md:mr-10">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             Hello, I'm <span className="text-blue-500">Rido!</span>
           </h2>
@@ -26,9 +26,9 @@ const Home = () => {
           </p>
 
           <div>
-            <button className="group hidden text-white w-fit px-6 py-2 my-2 lg:flex items-center rounded-md bg-gradient-to-l from-cyan-500 to-blue-500 cursor-pointer">
+            <button className="group text-white w-fit px-6 py-2 my-2 lg:flex items-center rounded-md bg-gradient-to-l from-cyan-500 to-blue-500 cursor-pointer">
             <Link to="about" smooth duration={500}>Get to know me!</Link>
-              <span className="group-hover:rotate-90 duration-300">
+              <span className="hidden sm:flex group-hover:rotate-90 duration-300">
                 <MdKeyboardArrowRight size={18}/>
               </span>
             </button>
